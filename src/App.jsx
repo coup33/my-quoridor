1d94eff3c0de4e86ab11d003e36b929de53f743f
coup33 <coup33@gmail.com>
2025-12-19 00:41:32 +0900

    fix: update server url for production

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { io } from 'socket.io-client';
 import './App.css';
 
-const socket = io('https://my-quoridor.vercel.app/');
+const socket = io('https://my-quoridor.onrender.com');
 
 function App() {
   const initialState = {
